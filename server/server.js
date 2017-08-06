3c0b3097691bb9d3cb91723edab478154edd14dc
Shawn Farsai <shawnfarsai@Shawns-MacBook-Pro.local>
2017-08-05 19:32:17 -0700

    debug

--- server/server.js
+++ server/server.js
@@ -114,6 +114,7 @@ app.post('/users/upload/id/:userId', (req, res) => {
   if (!req.files){
     return res.status(400).send('No files were uploaded.');
   } else {
+    console.log(idFile);
     let idFile = req.files.idFile.data;
     console.log(idFile);
 
